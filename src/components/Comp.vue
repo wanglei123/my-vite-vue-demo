07eb5fe91d7f0d028df0b1b6476e3adb3a670c4f
wanglei <806416060@qq.com>
2021-02-20 16:03:40 +0800

    添加setup jsx用法

--- src/components/Comp.vue
+++ src/components/Comp.vue
@@ -1,8 +1,20 @@
-<template>
-    <div>comp</div>
-</template>
-<script>
+
+<script lang="jsx">
+import { ref } from 'vue'
+
 export default {
+    setup(){
+        const counter = ref(0)
+        const onclick = () => {
+            counter.value++
+        }
+        return () => (
+            <>
+            <div>comp</div>
+            <p onclick={onclick}>{counter.value}</p>
+            </>
+        )
+    }
     
 }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 只能解析vue的单文件组件中的jsx,不能解析扩展名是jsx和tsx的文件
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(), vueJsx()]
 })

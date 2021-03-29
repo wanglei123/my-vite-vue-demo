913c357534250d1693d585229dac78e97dfa1120
wanglei <806416060@qq.com>
2021-03-29 14:12:43 +0800

    增加vuex

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <img alt="Vue logo" height="1000" src="./assets/logo.png" />
+  <!-- 传统写法 -->
+  <p @click="$store.commit('add')">{{$store.state.counter}}</p>
+  <!-- composition 写法 -->
+  <p @click="$store.commit('add')">{{state.counter}}</p>
   <!-- <router-view></router-view> -->
   <router-view v-slot="{Component}">
         <keep-alive>
@@ -12,13 +16,18 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import {ref} from 'vue'
+import {useStore} from 'vuex'
 
 const hw = ref(null)
+
+const {state} = useStore();
 
 const onmyclick = () => {
   console.log('这是从HelloWorld组件派发的事件');
   hw.value.someMethod()
 }
+
+
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
